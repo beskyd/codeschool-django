a7fc29198ff9fd1cd2c2f81bc0b32bac302cd42e
Ay Beskyd <beskydmail@gmail.com>
2017-04-24 21:32:49 +0000

    use context in index.html

--- try_django/challenges/TravelTracker/main/views.py
+++ try_django/challenges/TravelTracker/main/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
-def index(request):
-    name = 'Gold Mugget'
-    value = 1000.00
-    context = {
-        'treasure_name': name,
-        'treasure_val': value
-    }
-    return render(request, 'index.html', context)
+def home(request):
+    return HttpResponse('Welcome Home Travelers!')
