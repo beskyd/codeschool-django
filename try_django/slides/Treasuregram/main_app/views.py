7b39024f3f8cf42fba4ba54da21cf1f259703bc0
Ay Beskyd <beskydmail@gmail.com>
2017-04-25 16:20:19 +0000

    add img_url to the Treasure class. Add images of treasures to index.html

--- try_django/slides/Treasuregram/main_app/views.py
+++ try_django/slides/Treasuregram/main_app/views.py
@@ -7,14 +7,15 @@ def index(request):
     
     
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
         
 treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA')
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM", 'example.com/nugget.jpg'),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO", 'example.com/fools-gold.jpg'),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA', 'example.com/coffee-can.jpg')
 ]
