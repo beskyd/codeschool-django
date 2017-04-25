5172743dd82785102d5667a5b11a39c25735615a
Ay Beskyd <beskydmail@gmail.com>
2017-04-25 13:23:09 +0000

    create Location class in views.py

--- try_django/challenges/TravelTracker/main/views.py
+++ try_django/challenges/TravelTracker/main/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 
 def home(request):
-    location_name = "Fool's Falls"
-    predators = 'Scorpions, Snakes'
-    context = {
-        'location_name': location_name,
-        'predators': predators
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
+
+
+class Location:
+    def __init__(self, name, predators, num_restaurants, img_url):
+        self.name = name
+        self.predators = predators
+        self.num_restaurants = num_restaurants
+        self.img_url = img_url
