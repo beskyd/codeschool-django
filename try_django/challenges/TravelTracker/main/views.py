7e24f24c24ca9cdebc4cde3e9567fdf883ba90e6
Ay Beskyd <beskydmail@gmail.com>
2017-04-25 13:35:30 +0000

    render a list of locations in home.html using django's template language for loop

--- try_django/challenges/TravelTracker/main/views.py
+++ try_django/challenges/TravelTracker/main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'locations': locations}
+    return render(request, 'home.html', context)
 
 
 class Location:
