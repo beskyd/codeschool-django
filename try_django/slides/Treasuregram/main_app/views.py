b28fa226c40cbefaafa0b45247e151f7330410bc
Ay Beskyd <beskydmail@gmail.com>
2017-04-26 12:11:35 +0000

    use QuerySet of the Treasure model instead of the Treasure class in views.py to render Treasure objects in index.html

--- try_django/slides/Treasuregram/main_app/views.py
+++ try_django/slides/Treasuregram/main_app/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from .models import Treasure
 
 # Create your views here.
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html' , { 'treasures': treasures })
     
     
-class Treasure:
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-        
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM", 'example.com/nugget.jpg'),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO", 'example.com/fools-gold.jpg'),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA', 'example.com/coffee-can.jpg')
-]
