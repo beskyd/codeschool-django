8646d5506e967e038f4f252d355f4cc0fc6c75b1
Ay Beskyd <beskydmail@gmail.com>
2017-04-26 16:48:27 +0000

    refactor views.py to use the Location model instead of Location class

--- try_django/challenges/TravelTracker/main/views.py
+++ try_django/challenges/TravelTracker/main/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from .models import Location
 
 def home(request):
-    context = {'locations': locations}
-    return render(request, 'home.html', context)
+    locations = Location.objects.all()
+    return render(request, 'home.html', {'locations': locations})
 
 
-class Location:
-    def __init__(self, name, predators, num_restaurants, img_url):
-        self.name = name
-        self.predators = predators
-        self.num_restaurants = num_restaurants
-        self.img_url = img_url
-        
-locations = [
-    Location("Fool's Falls, CO", 'Flash Floods', 0,
-             'http://courseware.codeschool.com/try_django/images/fools-falls.jpg'),
-    Location("Curly's Creek, NM", 'Rattle Snakes', 2,
-             'http://courseware.codeschool.com/try_django/images/curlys-creek.jpg'),
-    Location('The Delicate Arch, UT', 'Ghost Brigade', 5, 'http://courseware.codeschool.com/try_django/images/delicate-arch.jpg')
-]
