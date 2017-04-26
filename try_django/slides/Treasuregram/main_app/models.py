d4c9088e1467df10508e62fc7cf44460c66d6fda
Ay Beskyd <beskydmail@gmail.com>
2017-04-26 11:43:38 +0000

    add custom __str__ method to the Treasure model

--- try_django/slides/Treasuregram/main_app/models.py
+++ try_django/slides/Treasuregram/main_app/models.py
@@ -10,4 +10,5 @@ class Treasure(models.Model):
     location = models.CharField(max_length = 100)
     img_url = models.CharField(max_length = 100)
     
-    
+    def __str__(self):
+        return self.name
