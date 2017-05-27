d87cbb351c285842f05d6afc640e08cf72ea161a
Ay Beskyd <beskydmail@gmail.com>
2017-05-27 18:36:41 +0000

    change img_url field to img in the Location model and update urls and settings

--- digging_into_django/challenges/TravelTracker/main/models.py
+++ digging_into_django/challenges/TravelTracker/main/models.py
@@ -7,7 +7,8 @@ class Location(models.Model):
     name = models.CharField(max_length = 100)
     predators = models.CharField(max_length = 100)
     num_restaurants = models.IntegerField()
-    img_url = models.CharField(max_length = 100)
+    img = models.ImageField(upload_to="location_images", default="location_images/default.png")
+
     
     def __str__(self):
         return self.name
--- digging_into_django/challenges/TravelTracker/main/urls.py
+++ digging_into_django/challenges/TravelTracker/main/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^([0-9]+)/$', views.detail, name = 'detail'),
     url(r'^post_location/$', views.post_location, name = 'post_location'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+            url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+        ]
