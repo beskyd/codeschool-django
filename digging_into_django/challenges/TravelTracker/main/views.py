4ac0413205c3894c8d0178234b54913313941922
Ay Beskyd <beskydmail@gmail.com>
2017-05-27 17:24:17 +0000

    edit home view to include a form rendering

--- digging_into_django/challenges/TravelTracker/main/views.py
+++ digging_into_django/challenges/TravelTracker/main/views.py
@@ -5,7 +5,8 @@ from .forms import LocationForm
 
 def home(request):
     locations = Location.objects.all()
-    return render(request, 'home.html', {'locations': locations})
+    form = LocationForm()
+    return render(request, 'home.html', {'locations': locations, 'form': form})
 
 def detail(request, location_id):
     location = Location.objects.get(id=location_id)
@@ -15,9 +16,8 @@ def post_location(request):
     form = LocationForm(request.POST)
     if form.is_valid():
         location = Location(name = form.cleaned_data['name'],
-                           num_restaurants = form.cleaned_data['num_restaurants'],
-                           predators = form.cleaned_data['predators'],
-                           img_url = form.cleaned_data['img_url'])
+                            num_restaurants = form.cleaned_data['num_restaurants'],
+                            predators = form.cleaned_data['predators'],
+                            img_url = form.cleaned_data['img_url'])
         location.save()
-    
     return HttpResponseRedirect('/')
