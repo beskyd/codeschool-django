531dc724dc70ff3c233507ff3644b6d8320fddb5
Ay Beskyd <beskydmail@gmail.com>
2017-05-27 18:23:47 +0000

    add special url to serve media files locally

--- digging_into_django/slides/Treasuregram/main_app/urls.py
+++ digging_into_django/slides/Treasuregram/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 # note that you can write '.' if importing a module from the current app
+from django.conf import settings
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -8,3 +10,8 @@ urlpatterns = [
     url(r'^([0-9]+)/$', views.detail, name='detail'),
     url(r'^post_url/$', views.post_treasure, name='post_treasure')
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+            url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),
+        ]
