a0b1e0fa800bd4bbed38568944c1309818c64702
Ay Beskyd <beskydmail@gmail.com>
2017-05-27 14:14:34 +0000

    add the detail view

--- digging_into_django/challenges/TravelTracker/main/views.py
+++ digging_into_django/challenges/TravelTracker/main/views.py
@@ -6,4 +6,7 @@ def home(request):
     locations = Location.objects.all()
     return render(request, 'home.html', {'locations': locations})
 
+def detail(request, location_id):
+    location = Location.objects.get(id=location_id)
+    return render(request, 'detail.html', {'location': location})
 
