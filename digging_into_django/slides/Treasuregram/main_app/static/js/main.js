e73b16fdfa2f2b405e6ecb24edc65c3bb399de92
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 10:33:52 +0000

    add ajax request to main.js to handle like button tapping

---
+++ digging_into_django/slides/Treasuregram/main_app/static/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $("button").on('click', function(event) {
+    event.preventDefault();
+    var element = $(this);
+    
+    $.ajax({
+        url: '/like_treasure/',
+        type: 'GET',
+        data: {'treasure_id': element.attr('data-id')},
+    })
+
+        
+});    
+
+    
+});
+
