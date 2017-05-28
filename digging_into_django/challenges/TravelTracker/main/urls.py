5c1641b7de792b9e48c421937d106a89ea0574c1
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 09:04:53 +0000

    add login_view and logout_view and links to base.html

--- digging_into_django/challenges/TravelTracker/main/urls.py
+++ digging_into_django/challenges/TravelTracker/main/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^([0-9]+)/$', views.detail, name = 'detail'),
     url(r'^post_location/$', views.post_location, name = 'post_location'),
     url(r'^user/(\w+)/$', views.profile, name='profile'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^register/$', views.register, name='register'),
 ]
 
--- digging_into_django/challenges/TravelTracker/main/views.py
+++ digging_into_django/challenges/TravelTracker/main/views.py
@@ -39,3 +39,33 @@ def register(request):
     else:
         form = UserCreationForm()
         return render(request, 'registration.html', {'form': form})
+        
+        
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                # the password verified for the user
+                if user.is_active:
+                    print("User is valid, active and authenticated")
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                    #return Index(request)
+                else:
+                    print("The password is valid, but the account has been disabled!")
+            else:
+                # the authentication system was unable to verify the username and password
+                print("The username and password were incorrect.")
+    else:
+        form = LoginForm()
+        request.user = User.objects.create_user('CodeSchool')
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
