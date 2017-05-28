08370044f76a428977b8b740726bc24bd766563c
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 07:56:00 +0000

    edit the Location model to include foreign key to the built-in User model

--- digging_into_django/challenges/TravelTracker/main/models.py
+++ digging_into_django/challenges/TravelTracker/main/models.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Location(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length = 100)
     predators = models.CharField(max_length = 100)
     num_restaurants = models.IntegerField()
