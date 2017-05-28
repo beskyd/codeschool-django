6d14f64c6ddedb4798b5d1cf065574db0ea7779a
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 07:33:41 +0000

    create url for user profile display

--- digging_into_django/slides/Treasuregram/main_app/urls.py
+++ digging_into_django/slides/Treasuregram/main_app/urls.py
@@ -8,7 +8,8 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^([0-9]+)/$', views.detail, name='detail'),
-    url(r'^post_url/$', views.post_treasure, name='post_treasure')
+    url(r'^post_url/$', views.post_treasure, name='post_treasure'),
+    url(r'^user/(\w+)/$', views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
