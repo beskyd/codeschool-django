278fcfc20a9e5126ebc0f62b352e72ddc706d96c
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 10:59:31 +0000

    add search handling with ajax in main.js

---
+++ digging_into_django/challenges/TravelTracker/main/static/js/main.js
@@ -0,0 +1,12 @@
+$('#search-form').on('submit', function(event){
+    event.preventDefault();
+
+    $.ajax({
+        url: '/search/',
+        method: 'GET',
+        data: {search: $('#search-text').val()},
+        success: function(json) {
+            alert(JSON.stringify(json));
+        }
+    });
+});
