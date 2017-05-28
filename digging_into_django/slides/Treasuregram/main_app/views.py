7866276ea17ea0df5c7e1901d5189627cd2c1d64
Ay Beskyd <beskydmail@gmail.com>
2017-05-28 08:30:59 +0000

    add the login_view

--- digging_into_django/slides/Treasuregram/main_app/views.py
+++ digging_into_django/slides/Treasuregram/main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Treasure
-from .forms import TreasureForm
+from .forms import TreasureForm, LoginForm
 
 
 # Create your views here.
@@ -40,3 +41,22 @@ def profile(request, username):
     treasures =  Treasure.objects.filter(user=user)
     return render(request, 'profile.html', {'username': username, 'treasures': treasures})
     
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active():
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled!")
+            else:
+                print("The username or password is incorrect.")
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
